a90a9f65c2a9d0cd6c7d2b8ee510c0fc5fe05459
yuyun.xyy <yuyun.xyy@alibaba-inc.com>
2022-01-27 01:46:23 -0800

    fix code

--- coroutineTest.go
+++ coroutineTest.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 func testIo() {
@@ -9,23 +12,39 @@ func testIo() {
 
 	go func() {
 		println(" start baidu ")
-		resp, err := http.Get("http://go.dev/")
-		println("baidu status code = ",resp.StatusCode)
+		resp, err := http.Get("http://baidu.com/")
 		if err != nil {
 			println("error is ", err.Error())
+			return
 		}
+		println("baidu status code = ",resp.StatusCode)
 	}()
 
+	//go func() {
+	//	println(" start google ")
+	//	resp, err := http.Get("http://google.com")
+	//	println("google status code = ",resp.StatusCode)
+	//	defer resp.Body.Close()
+	//	body, err := ioutil.ReadAll(resp.Body)
+	//	fmt.Println(string(body))
+	//	if err != nil {
+	//		println("error is ", err.Error())
+	//	}
+	//
+	//}()
+	//
 	go func() {
-		println(" start google ")
-		resp, err := http.Get("http://google.com")
-		println("google status code = ",resp.StatusCode)
-		//defer resp.Body.Close()
-		//body, err := ioutil.ReadAll(resp.Body)
-		//fmt.Println(string(body))
+		println(" start google timeout test")
+		client := http.Client{Timeout: 10 * time.Second}
+		resp, err :=client.Get("http://google.com");
 		if err != nil {
 			println("error is ", err.Error())
+			return
 		}
+		println("google status code = ",resp.StatusCode)
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Println(string(body))
 
 	}()
 }
